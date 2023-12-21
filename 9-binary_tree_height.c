5a577288515694a3b1bc8fd650c7c2ff5222ab14
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 11:37:09 -0600

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,22 +1,69 @@
-#include "binary_trees.h"
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree.
- *
- * Return: The height of the tree, or 0 if the tree is empty.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;  /* Declare variables at the beginning */
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-	if (!tree)
-		return (0);  /* Base case: Empty tree has height 0 */
+// Define the binary tree node structure
+typedef struct binary_tree_s {
+    int n;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+// Function to measure the height of a binary tree
+size_t binary_tree_height(const binary_tree_t *tree) {
+    size_t count_left, count_right;
 
-	/* Return the larger height plus 1 to account for the current node */
-	return (0 + (left_height > right_height ? left_height : right_height));
+    if (!tree || (!tree->left && !tree->right))
+        return (0);
+
+    count_left = binary_tree_height(tree->left);
+    count_right = binary_tree_height(tree->right);
+
+    return (MAX(count_left, count_right) + 1);
+}
+
+// Function to create a new binary tree node
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+    binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+    if (newNode == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->n = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+int main() {
+    binary_tree_t *root = binary_tree_node(NULL, 50);
+
+    // Build a sample binary tree
+    root->left = binary_tree_node(root, 30);
+    root->right = binary_tree_node(root, 70);
+
+    root->left->left = binary_tree_node(root->left, 20);
+    root->left->right = binary_tree_node(root->left, 40);
+
+    root->right->left = binary_tree_node(root->right, 60);
+    root->right->right = binary_tree_node(root->right, 80);
+
+    // Measure the height of the binary tree
+    size_t height = binary_tree_height(root);
+
+    printf("Height of the binary tree: %lu\n", height);
+
+    // Free the memory allocated for the binary tree nodes
+    free(root->left->left);
+    free(root->left->right);
+    free(root->right->left);
+    free(root->right->right);
+    free(root->left);
+    free(root->right);
+    free(root);
+
+    return 0;
 }
