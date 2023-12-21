d03785fbe283345effd880bfd23a499465b6180d
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 09:52:47 -0600

    maybe

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,17 +1,17 @@
-#include <stddef.h>
 #include "binary_trees.h"
+#include <stddef.h>
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    size_t left_height, right_height;  /* Declare variables at the very beginning */
+
     if (tree == NULL) {
         return 0;  /* Base case: Empty tree has height 0 */
     }
 
-    size_t left_height, right_height;  /* Declare both variables at the beginning */
-
     left_height = binary_tree_height(tree->left);
     right_height = binary_tree_height(tree->right);
 
-    // Return the larger height plus 1 to account for the current node
+    /* Return the larger height plus 1 to account for the current node */
     return 1 + (left_height > right_height ? left_height : right_height);
 }
