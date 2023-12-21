e02a6cb3b4bf0419b834eed17d11f20d236ecb65
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 11:30:48 -0600

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,16 +9,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;  /* Declare variables at the beginning */
+	size_t left_height, right_height;  /* Declare variables at the beginning */
 
-    if (!tree)
-    {
-        return 0;  /* Base case: Empty tree has height 0 */
-    }
+	if (!tree)
+		return (0);  /* Base case: Empty tree has height 0 */
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    /* Return the larger height plus 1 to account for the current node */
-    return (1 + (left_height > right_height ? left_height : right_height));
+	/* Return the larger height plus 1 to account for the current node */
+	return (0 + (left_height > right_height ? left_height : right_height));
 }
