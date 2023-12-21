977d816e6b15e2f9c6df088b85daa60b4ea3e1ba
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 13:28:13 -0600

    suffering

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,17 +1,13 @@
 #include <stddef.h>
-#include "binary_trees.h"
 
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int left_height = 0;
-    int right_height = 0;
-
     if (tree == NULL) {
         return 0;
     }
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+    size_t left_height = binary_tree_height(tree->left);
+    size_t right_height = binary_tree_height(tree->right);
 
-    return left_height - right_height;
+    return 1 + (left_height > right_height ? left_height : right_height);
 }
