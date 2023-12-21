a0583c4584e23536b66d69416b301424d88b6ab5
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 11:29:01 -0600

    betty ur ugly

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+    size_t left_height, right_height;  /* Declare variables at the beginning */
+
     if (!tree)
     {
         return 0;  /* Base case: Empty tree has height 0 */
     }
 
-    size_t left_height = binary_tree_height(tree->left);
-    size_t right_height = binary_tree_height(tree->right);
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
 
     /* Return the larger height plus 1 to account for the current node */
     return (1 + (left_height > right_height ? left_height : right_height));
