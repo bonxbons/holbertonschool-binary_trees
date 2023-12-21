d4013cded6f190aa7ecc927758440f46954dd374
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-21 13:48:09 -0600

    kms

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,10 +11,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	size_t left_height, right_height;
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
